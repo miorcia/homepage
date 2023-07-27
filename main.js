bce55962e1f03b3fd1e4232bddda2bd24a6a6b69
Emi <emilia.olejniczak.1993@gmail.com>
2023-07-27 20:23:13 +0200

    BEM and DOM

--- main.js
+++ main.js
@@ -2,4 +2,18 @@ const firstName = 'Emi';
 const age = 30;
 console.log(firstName, age);
 
-console.log(`Nazywam się ${firstName} i mam ${age} lat`)
+console.log(`Nazywam się ${firstName} i mam ${age} lat`);
+
+const header = document.querySelector('h1');
+
+console.log(header);
+
+const czerwien = document.querySelector('.czerwony');
+
+console.log(czerwien);
+
+header.innerHTML = 'Co żem frontend';
+
+console.log(header.style);
+
+header.style.color = 'red';
